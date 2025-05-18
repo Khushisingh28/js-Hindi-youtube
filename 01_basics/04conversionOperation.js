057381a058988bc0c9d0416ffe2e737dd68dc1e8
khushi singh <28khushisingh.01@gmail.com>
2025-05-18 17:32:30 +0000

    coversion

---
+++ 01_basics/04conversionOperation.js
@@ -0,0 +1,11 @@
+let score = 33
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let khushi = "33abc"
+let valueInNumber = Number(khushi);
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+let cat = null
