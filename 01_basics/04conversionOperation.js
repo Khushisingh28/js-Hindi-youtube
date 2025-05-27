98832e465055c72d33d052c61d1701ffe576a23d
khushi singh <28khushisingh.01@gmail.com>
2025-05-27 06:19:44 +0000

    Conversion

--- 01_basics/04conversionOperation.js
+++ 01_basics/04conversionOperation.js
@@ -1,11 +1,47 @@
-let score = 33
-
+let score = "33"
 console.log(typeof score);
 console.log(typeof(score));
+let a = Number(score)
+console.log(typeof(a))
+console.log(a)
 
 let khushi = "33abc"
 let valueInNumber = Number(khushi);
 console.log(typeof valueInNumber);
 console.log(valueInNumber)
 
+// "33" => 33
+//"33abc" => NaN
+
 let cat = null
+console.log(typeof(cat))
+let bat = Number(cat)
+console.log(typeof(bat))
+console.log(bat)
+
+let sat = undefined
+console.log(typeof(sat))
+let aat = Number(sat)
+console.log(typeof(aat))
+console.log(aat)
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let Logged = ""
+let booleanLogged = Boolean(Logged)
+console.log(booleanLogged);
+
+let isLogged = "Khushi"
+let booleanIsLogged = Boolean(isLogged)
+console.log(booleanIsLogged);
+
+// 1-> true; 0 => false
+// "" => false
+// "khushi"=> true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof(stringNumber))
+console.log(stringNumber)
