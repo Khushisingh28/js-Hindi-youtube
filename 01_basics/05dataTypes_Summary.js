15b5af75235de1a07ff703c7e5e84fe6ae6c125d
khushi singh <28khushisingh.01@gmail.com>
2025-05-29 17:36:00 +0000

    new

---
+++ 01_basics/05dataTypes_Summary.js
@@ -0,0 +1,24 @@
+//  Primitive
+
+// 7 types : String,Number,Boolean,null,undefined,Symbol,BigInt
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id===anotherId)
+;
+// Reference (Non Primitive)
+
+// Array,Objects,Functions
+
+// Array
+
+const heros = ["Khushi","is","Best"];
+
+// Objects
+
+let myObj = {
+    name : "Khushi",
+    age :22,
+}
