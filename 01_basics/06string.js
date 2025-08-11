37c4f876cd86f973522924ebe2ec522a25e2ef30
khushi singh <28khushisingh.01@gmail.com>
2025-08-11 14:54:49 +0000

    string

---
+++ 01_basics/06string.js
@@ -0,0 +1,15 @@
+const name = "khushi";
+const repocount = 6
+
+//console.log(name + repocount + " value");
+
+console.log(`Hello my name is ${name} and my repocount is ${repocount}`);
+console.log(` ${name}  ${repocount}`);
+
+const gameName = new String("Khushi");
+
+console.log(gameName[0]);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+
+
