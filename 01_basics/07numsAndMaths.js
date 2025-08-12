8ce3798af961e182f435c49166633b7041386c49
khushi singh <28khushisingh.01@gmail.com>
2025-08-12 07:23:54 +0000

    number

---
+++ 01_basics/07numsAndMaths.js
@@ -0,0 +1,20 @@
+const score = 400;
+console.log(score);
+
+const balance = new Number(100);
+console.log(balance);
+
+console.log(balance.toString().length);
+
+const string = balance.toString();
+console.log(string);
+console.log(string.length);
+
+console.log(balance.toFixed(2));
+
+const otherNumber = 23.89966;
+console.log(otherNumber.toPrecision(3))
+
+const numberTwo = 123.8966;
+console.log(numberTwo.toPrecision(3))
+
